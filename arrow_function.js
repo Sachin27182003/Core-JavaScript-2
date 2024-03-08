4f93c24676a03c97bfd56db35d3c7709ef31ae78
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2024-03-09 00:54:59 +0530

    Square of any number

---
+++ arrow_function.js
@@ -0,0 +1,11 @@
+
+
+/* Ceate an arrow function called square that takes a number as an agument and retuns its square. Use
+the arrow function to calculate the square of a given number and display the result 
+*/
+
+sum = (a) => {
+    return a*a;
+}
+
+console.log(sum(6));
